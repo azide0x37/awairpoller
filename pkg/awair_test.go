b42b954de93b34df436e387f7447e456a5783beb
Alexander Templeton <blakmatr@gmail.com>
2021-06-17 19:36:38 -0500

    adding test

--- pkg/awair_test.go
+++ pkg/awair_test.go
@@ -1,13 +1,51 @@
 package awairpoller
 
-import "testing"
+import (
+	"testing"
 
-func TestAwairPoller_Validate(t *testing.T) {
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/apps/v1"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestAwairPoller_Validate_Nil_Deployment(t *testing.T) {
 	y := New()
 	y.deployment = nil
 	err := y.Validate()
 	if err == nil {
 		t.Errorf("Expecting error for nil deployment")
+	}
+}
+
+func TestAwairPoller_Validate_Zero_Containers(t *testing.T) {
+	y := New()
+	y.deployment = &v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "awair-poller",
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: int32Ptr(2),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"beeps": "boops",
+				},
+			},
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"beeps": "boops",
+					},
+				},
+				Spec: apiv1.PodSpec{
+					Containers: []apiv1.Container{},
+				},
+			},
+		},
+	}
+	err := y.Validate()
+	if err == nil {
+		t.Errorf("Expecting error for no containers")
 	}
 }
 
