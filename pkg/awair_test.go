f2e051b695d68c1d84c7ccc7f8c3d3d17f8acb07
Alexander Templeton <blakmatr@gmail.com>
2021-06-17 21:42:21 -0500

    autogen test cases

--- pkg/awair_test.go
+++ pkg/awair_test.go
@@ -1,12 +1,14 @@
 package awairpoller
 
 import (
+	"reflect"
 	"testing"
 
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 func TestAwairPoller_Validate_Nil_Deployment(t *testing.T) {
@@ -91,5 +93,83 @@ func TestAwairPoller_ArchiveNilDeployment(t *testing.T) {
 	err := y.Archive()
 	if err == nil {
 		t.Errorf("Expecting error for nil deployment")
+	}
+}
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name string
+		want *AwairPoller
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := New(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("New() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAwairPoller_KubernetesClient(t *testing.T) {
+	type fields struct {
+		ContainerPort  int32
+		ContainerImage string
+		client         *kubernetes.Clientset
+		deployment     *v1.Deployment
+	}
+	type args struct {
+		kubeconfigPath string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			y := &AwairPoller{
+				ContainerPort:  tt.fields.ContainerPort,
+				ContainerImage: tt.fields.ContainerImage,
+				client:         tt.fields.client,
+				deployment:     tt.fields.deployment,
+			}
+			if err := y.KubernetesClient(tt.args.kubeconfigPath); (err != nil) != tt.wantErr {
+				t.Errorf("AwairPoller.KubernetesClient() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestAwairPoller_UninstallKubernetes(t *testing.T) {
+	type fields struct {
+		ContainerPort  int32
+		ContainerImage string
+		client         *kubernetes.Clientset
+		deployment     *v1.Deployment
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			y := &AwairPoller{
+				ContainerPort:  tt.fields.ContainerPort,
+				ContainerImage: tt.fields.ContainerImage,
+				client:         tt.fields.client,
+				deployment:     tt.fields.deployment,
+			}
+			if err := y.UninstallKubernetes(); (err != nil) != tt.wantErr {
+				t.Errorf("AwairPoller.UninstallKubernetes() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
 	}
 }
